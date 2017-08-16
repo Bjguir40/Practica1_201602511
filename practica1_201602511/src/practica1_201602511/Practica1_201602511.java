7f7997004da92675bc18cdf1d4966c4ddbccb42c
Byron G <Byron G@Byron>
2017-08-15 18:29:43 -0600

    menus y matrices

---
+++ practica1_201602511/src/practica1_201602511/Practica1_201602511.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_201602511;
+
+/**
+ *
+ * @author Byron G
+ */
+public class Practica1_201602511 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        menu_201602511 principal = new menu_201602511();
+        principal.menu1();
+    }
+    
+}
